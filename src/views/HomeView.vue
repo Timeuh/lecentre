b9931c3dcdb95c8f4ea15d6b196fb3cbf9b76131
Timeuh <timeuuh@gmail.com>
2026-01-06 14:41:01 +0100

    🚧 display cars in home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
+<script setup>
+import { cars } from '@/data/cars'
+</script>
+
 <template>
-  <main>
-    <h1>Le Centre</h1>
+  <main
+    class="w-full min-h-screen h-fit bg-gray-200 flex flex-col items-center space-y-12 px-[5vw]"
+  >
+    <h1 class="text-4xl pt-12 text-red-700 font-bold">LeCentre</h1>
+    <section class="w-full grid grid-cols-5 gap-6">
+      <div v-for="car in cars">
+        <h2>{{ car.brand }} {{ car.model }}</h2>
+      </div>
+    </section>
   </main>
 </template>
