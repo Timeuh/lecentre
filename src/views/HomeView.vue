528cf14f769dd2569ef54cb1a80c200937cf7f43
Timeuh <timeuuh@gmail.com>
2026-01-07 12:45:32 +0100

    💄 remove title and add top spacing

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,8 @@ import CarPreview from '@/components/CarPreview.vue'
 
 <template>
   <main
-    class="w-full min-h-screen h-fit bg-gray-200 flex flex-col items-center space-y-12 px-[5vw]"
+    class="w-full min-h-screen h-fit bg-gray-200 flex flex-col items-center space-y-12 px-[5vw] pt-20"
   >
-    <h1 class="text-4xl pt-12 text-red-700 font-bold">LeCentre</h1>
     <section class="w-full grid grid-cols-5 gap-12 justify-center">
       <div v-for="car in cars">
         <CarPreview :car="car" />
