90d9e073a8b1a2bd7f6fe96e6a01d622daf64539
Timeuh <timeuuh@gmail.com>
2026-01-07 14:32:04 +0100

    🚧 create searchbar

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="rounded-full border-2 border-gray-700 p-2 w-3/5 mt-8">
+    <input
+      type="text"
+      placeholder="Marque ou modèle"
+      class="border-r-2 border-gray-600 w-[70%] ps-2 outline-none"
+    />
+    <select name="department" id="department" class="outline-none w-[30%] px-4">
+      <option value="none">Aucun filtre</option>
+    </select>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import CarPreview from '@/components/CarPreview.vue'
+import SearchBar from '@/components/SearchBar.vue'
 import { onMounted } from 'vue'
 import { useCarStore } from '@/stores/carStore'
 import { storeToRefs } from 'pinia'
@@ -24,6 +25,7 @@ onMounted(() => {
   <main
     class="w-full min-h-screen h-fit bg-gray-200 flex flex-col items-center space-y-12 px-[5vw] pt-20"
   >
+    <SearchBar />
     <section class="w-full grid grid-cols-5 gap-12 justify-center">
       <div v-for="(car, index) in carsToDisplay">
         <CarPreview :car="car" :is-favorite="favorites.includes(index)" :listing-index="index" />
