6371edd5a51da0a762450e73bc9ca3b68d027d4d
Timeuh <timeuuh@gmail.com>
2026-01-07 15:33:28 +0100

    ✨ persist favorites in local storage

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,9 +1,24 @@
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
   // user favorite listings
   const favorites = ref<Array<number>>([])
+
+  // get previous favorites from local storage
+  const stored = localStorage.getItem('favorites')
+  if (stored) {
+    favorites.value = JSON.parse(stored)
+  }
+
+  // when favorites changes, update the local storage
+  watch(
+    favorites,
+    (value) => {
+      localStorage.setItem('favorites', JSON.stringify(value))
+    },
+    { deep: true },
+  )
 
   /**
    * Add a car listing to favorites
