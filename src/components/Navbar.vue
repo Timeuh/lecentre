67333ef96c31b42307555d95cc0e1a32ef5fbcdf
Timeuh <timeuuh@gmail.com>
2026-01-07 14:13:39 +0100

    🩹 put navbar on higher z-index to prevent cliping

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@ import { RouterLink } from 'vue-router'
 
 <template>
   <nav
-    class="fixed top-0 flex flex-row items-center justify-between w-full bg-gray-200 p-4 px-6 shadow-lg shadow-gray-700 text-xl"
+    class="fixed z-10 top-0 flex flex-row items-center justify-between w-full bg-gray-200 p-4 px-6 shadow-lg shadow-gray-700 text-xl"
   >
     <h1 class="text-2xl text-red-700 font-bold">LeCentre</h1>
     <div class="flex flex-row space-x-12 items-center">
