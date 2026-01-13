6e1613ea219ac3879fbf9e37ecb41b13ef2315f9
Timeuh <timeuuh@gmail.com>
2026-01-13 12:22:49 +0100

    🔥 remove searchbar from favorites page

--- src/views/FavoriteView.vue
+++ src/views/FavoriteView.vue
@@ -2,7 +2,6 @@
 import { useUserStore } from '@/stores/userStore'
 import { useCarStore } from '@/stores/carStore'
 import { storeToRefs } from 'pinia'
-import SearchBar from '@/components/SearchBar.vue'
 import CarPreview from '@/components/CarPreview.vue'
 import type { Car } from '@/types/app-types'
 
@@ -23,7 +22,6 @@ const favoriteCars: Array<Car> = carsToDisplay.value.filter((car: Car) => {
   <main
     class="w-full min-h-screen h-fit bg-gray-200 flex flex-col items-center space-y-12 px-[5vw] pt-20"
   >
-    <SearchBar />
     <section class="w-full grid grid-cols-5 gap-12 justify-center">
       <div v-for="car in favoriteCars">
         <CarPreview :car="car" :is-favorite="favorites.includes(car.id)" />
