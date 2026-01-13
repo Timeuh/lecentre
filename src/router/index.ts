73fd7449325316577fcc8b31908065819f7c61d7
Timeuh <timeuuh@gmail.com>
2026-01-13 12:21:35 +0100

    🔧 declare vue components as modules

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import FavoriteView from '../views/FavoriteView.vue'
-import HomeView from '../views/HomeView.vue'
+import FavoriteView from '@/views/FavoriteView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
