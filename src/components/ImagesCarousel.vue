02fb4484d1b6d30432e4f46ff81c98af6dfa3b26
Timeuh <timeuuh@gmail.com>
2026-01-14 12:36:10 +0100

    🚧 create images carousel for car photos

---
+++ src/components/ImagesCarousel.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  images: Array<string>
+}>()
+
+const currentImage = ref<string>(props.images[0] ?? '')
+</script>
+
+<template>
+  <section>
+    <img :src="'/images/' + currentImage" :alt="currentImage" />
+    <div>
+      <img :src="'/images/' + images[0]" :alt="images[0]" />
+      <img :src="'/images/' + images[1]" :alt="images[1]" />
+      <img :src="'/images/' + images[2]" :alt="images[2]" />
+    </div>
+  </section>
+</template>
