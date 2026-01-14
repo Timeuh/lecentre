8cae4a151d7389941c7d2c538ad2c2db4af7c70f
Timeuh <timeuuh@gmail.com>
2026-01-14 12:11:19 +0100

    🚧 create route and page for car listing display

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import FavoriteView from '@/views/FavoriteView.vue'
 import HomeView from '@/views/HomeView.vue'
+import CarView from '@/views/CarView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/favorites',
       name: 'favorites',
       component: FavoriteView,
+    },
+    {
+      path: '/car/:id',
+      name: 'car',
+      component: CarView,
     },
   ],
 })
---
+++ src/views/CarView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCarStore } from '@/stores/carStore'
+import { storeToRefs } from 'pinia'
+
+// get car store
+const carStore = useCarStore()
+const { carsToDisplay } = storeToRefs(carStore)
+</script>
+
+<template>
+  <main
+    class="w-full min-h-screen h-fit bg-gray-200 flex flex-col items-center space-y-12 px-[5vw] pt-20"
+  >
+    <h1>Voiture {{ $route.params.id }}</h1>
+  </main>
+</template>
